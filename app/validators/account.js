a0343527ca59c6a579480a85f5064bfc5fb6e2a6
Ubuntu <mgonzalez@nautilusdigital.com>
2019-02-06 23:20:40 +0000

    added gal prefix in regular expression

--- app/validators/account.js
+++ app/validators/account.js
@@ -1,6 +1,6 @@
 import { validateFormat } from 'ember-changeset-validations/validators';
 
-export const regex = /^(xrb|nano)(_|-)[13]((?![lv])[a-z1-9]){59}$/;
+export const regex = /^(xrb|nano|gal)(_|-)[13]((?![lv])[a-z1-9]){59}$/;
 
 export default function validateAccount(options = {}) {
   return validateFormat(Object.assign({}, options, { regex }));
